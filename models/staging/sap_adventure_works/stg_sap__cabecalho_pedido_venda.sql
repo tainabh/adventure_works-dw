9e74ba921aa4030d50b745129728e5e807bf9b39
Tainá Braúlio Hennig <hennigtain@gmail.com>
2024-01-20 14:35:23 +0000

    Criadas as stgs cabecalho_pedido_vendas, motivo_venda e cartao_credito

---
+++ models/staging/sap_adventure_works/stg_sap__cabecalho_pedido_venda.sql
@@ -0,0 +1,33 @@
+with
+    fonte_cabecalho_pedido as (
+        select 
+            cast (rowguid as string) as sk_pedido -- chave surrogate, identificador global de linha na tabela  
+            , cast (salesorderid as int) as id_pedido_venda
+            , cast (customerid as int) as id_cliente
+            , cast (salespersonid as int) as id_vendedor
+            , cast (territoryid as int) as id_territorio
+            , cast (billtoaddressid as int) as id_endereco_faturamento
+            , cast (shiptoaddressid as int) as id_endereco_entrega
+            , cast (shipmethodid as int) as id_forma_entrega
+            , cast (creditcardid as int) as id_cartao_credito
+            , cast (currencyrateid as int) as id_taxa_cambio
+            , cast (purchaseordernumber as string) as codigo_pedido
+            , cast (accountnumber as string) as numero_conta
+            , cast (revisionnumber as numeric) as numero_revisao
+            , cast (orderdate as string) as data_pedido
+            , cast (duedate as string) as prazo_entrega_pedido
+            , cast (shipdate as string) as data_envio
+            , cast (status as int) as status_pedido
+            , cast (onlineorderflag as string) as pedido_foi_online      
+            , cast (creditcardapprovalcode as string) as codigo_aprovacao_cartao_credito
+            , cast (subtotal as numeric) as subtotal_pedido
+            , cast (taxamt as numeric) as taxa_pedido
+            , cast (freight as numeric) as valor_frete
+            , cast (totaldue as numeric) as total_a_receber_pedido
+            --, comment            
+            --, modifieddate
+        from {{ source('sap', 'salesorderheader') }}
+    )
+
+select *
+from fonte_cabecalho_pedido
